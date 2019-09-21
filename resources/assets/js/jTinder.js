d0fa8b0558ccebe8c5eb7137be3610c2c2d74080
shinichi-ito <shinichi-ito@lotsjoys.com>
2019-09-21 22:28:51 +0900

    test

--- resources/assets/js/jTinder.js
+++ resources/assets/js/jTinder.js
@@ -6,7 +6,7 @@ var postReaction = function (to_user_id, reaction) {
     }
   });
   $.ajax({
-    type: "POST",  
+    type: "POST",  //vv
     url: "/api/like",
     data: {
       to_user_id: to_user_id,
